71bfe6d87ad48a46753ef224a1553c5b9f1e03f1
rexpiyum <rexpiyum@gmail.com>
2020-05-07 16:33:30 -0700

    fixing typo

--- backend_dream_drone/api/models/drone.js
+++ backend_dream_drone/api/models/drone.js
@@ -21,7 +21,7 @@ var droneSchema = new Schema({
 
   createdBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 
-  reactions: [type.String],
+  reactions: [{type: String}],
 
   isFlagged: {
     type: Boolean,
